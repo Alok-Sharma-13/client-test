d802ef572f1902a3899ecb2c6c489a5721df6c74
Alok_Sharma <aloksharma132005@gmail.com>
2026-04-03 19:41:15 +0530

    change

--- app.py
+++ app.py
@@ -91,4 +91,4 @@ def upload():
 
 # ---------------- START SERVER ----------------
 if __name__ == "__main__":
-    app.run(port=5090, debug=True)
+    app.run(host="0.0.0.0", port=5090, debug=True)
